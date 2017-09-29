a9355ead134dbd62d05166f25d646c2219b612dd
youngky hermanto <yhermanto@gmail.com>
2017-09-29 06:06:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,3 +6,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+}
